ad32b71d42959e34837145cd7d4a7a0c56a62f06
Documenter.jl <documenter@juliadocs.github.io>
2025-01-29 05:45:54 +0000

    build based on 1bd1f4e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NSInterpolate","category":"page"},{"location":"#NSInterpolate","page":"Home","title":"NSInterpolate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NSInterpolate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NSInterpolate]","category":"page"}]
+}
